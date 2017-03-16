8c98c97d275f8ee3268396a0e809476b8f702e5b
Michael A. Perlin <mika.perlin@gmail.com>
2017-03-16 17:13:26 -0600

    fixed submission to work off of the login node

--- submit-simulation.py
+++ submit-simulation.py
@@ -8,46 +8,31 @@ mkfac = project_dir + "/mkfac.py"
 job_dir = project_dir + "/jobs"
 simulate = "simulate.exe"
 
-# process inputs
-if len(sys.argv) < 2:
-    print("usage: {} [{}] walltime_in_hours" + \
-          " [simulation arguments]".format(sys.argv[0]),whide_flag)
-    exit(1)
-
 whide = whide_flag in sys.argv
 if whide: sys.argv.remove(whide_flag)
 
-time = sys.argv[1] + ":00:00"
+if len(sys.argv) < 2:
+    time = "02:00:00"
+else:
+    time = sys.argv[1] + ":00"
+
 sim_args = sys.argv[2:]
 
-on_rc_server = ("colorado.edu" in socket.getfqdn())
-rc_modules = [ "gcc/6.1.0", "openmpi/1.10.2", "boost/1.61.0", "python/3.5.1" ]
-rc_initialize = "module load " + " ".join(rc_modules)
-def rc_exec_list(cmd):
-    return [ "ssh", "scompile", "{}; cd {}; {}".format(rc_initialize, project_dir, cmd) ]
+simulate_cmd_list = [ "{}/{}".format(project_dir, simulate) ] + sim_args
 
-# get output of a command
-def get_output(cmd_list):
-    result = subprocess.run(cmd_list, stdout = subprocess.PIPE)
-    return result.stdout.decode("utf-8")
+on_rc_server = ("colorado.edu" in socket.getfqdn())
+rc_modules = [ "gcc/5.1.0", "openmpi/1.8.5", "boost/1.61.0" ]
+rc_initialize = "module load " + " ".join(rc_modules)
 
 # before proceeding further, build the project
 print("building project...")
-if on_rc_server:
-    subprocess.call([ mkfac, "no-build" ])
-    fac = get_output([ "which", "fac" ])
-    subprocess.call(rc_exec_list(fac))
-else:
-    subprocess.call([mkfac])
+if on_rc_server: subprocess.call(rc_initialize, shell = True)
+subprocess.call([mkfac])
 
 # determine simulation file basename
-suffix_cmd = [ "{}/{}".format(project_dir, simulate), "--suffix" ]
-if sim_args != []:
-    suffix_cmd += sim_args
-if on_rc_server:
-    suffix_cmd = rc_exec_list(" ".join(suffix_cmd))
-
-suffix = get_output(suffix_cmd).split()[-1]
+suffix_cmd_list = simulate_cmd_list + [ "--suffix" ]
+suffix_cmd_result = subprocess.run(suffix_cmd_list, stdout = subprocess.PIPE)
+suffix = suffix_cmd_result.stdout.decode("utf-8").split()[-1]
 basename = "network" + ".".join(suffix.split(".")[:-1])
 job_file = "{}/{}.sh".format(job_dir,basename)
 out_file = "{}/{}.out".format(job_dir,basename)
@@ -65,11 +50,10 @@ options = [ [ "output", out_file ],
 # construct and write job file
 job_text = "#!/usr/bin/env sh\n"
 for option in options:
-    job_text += "#SBATCH --{} {}\n".format(option[0],option[1])
+    job_text += "#SBATCH --{} {}\n".format(option[0], option[1])
 job_text += "\n"
-if on_rc_server:
-    job_text += rc_initialize + "\n"
-job_text += "{}/{} ".format(project_dir, simulate) + " ".join(sim_args) + "\n"
+if on_rc_server: job_text += rc_initialize + "\n"
+job_text += " ".join(simulate_cmd_list) + "\n"
 
 if not os.path.isdir(job_dir):
     os.makedirs(job_dir)
